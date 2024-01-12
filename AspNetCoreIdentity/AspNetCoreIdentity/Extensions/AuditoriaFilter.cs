ae298d6e1d4db667ee0b8ab48d5405e834a22c78
Gabriel Oliveira <gabriel.0008.vin.o@gmail.com>
2024-01-12 15:08:52 -0400

    Curso ASP.NET CORE Identity

---
+++ AspNetCoreIdentity/AspNetCoreIdentity/Extensions/AuditoriaFilter.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCoreIdentity.Controllers;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreIdentity.Extensions
+{
+    public class AuditoriaFilter : IActionFilter
+    {
+        private readonly ILogger<AuditoriaFilter> _logger;
+        public AuditoriaFilter(ILogger<AuditoriaFilter> logger)
+        {
+            _logger = logger;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var message = context.HttpContext.User.Identity.Name +
+                    " Acessou: " + context.HttpContext.Request.GetDisplayUrl() +
+                    " Request " + context.HttpContext.Request.ToString();
+
+                _logger.LogError(message);
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
---
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -0,0 +1,85 @@
+﻿using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Config;
+using AspNetCoreIdentity.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+ using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentity
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IHostingEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            
+
+            if (hostEnvironment.IsProduction())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddIdentityConfig(Configuration);
+
+            services.AddAuthorizationConfig();
+
+            services.ResolveDependencies();
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(typeof(AuditoriaFilter));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication(); // ou o Identity não funciona
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
