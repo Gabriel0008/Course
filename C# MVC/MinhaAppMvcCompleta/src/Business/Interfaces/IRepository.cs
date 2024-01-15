fd9e76a2e36fc854de8b79bb3dfb628b06f850cc
Gabriel Oliveira <gabriel.0008.vin.o@gmail.com>
2024-01-15 17:00:26 -0400

    Alterado Estruturas das Pastas

---
+++ C# MVC/MinhaAppMvcCompleta/src/Business/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity // Obriga que o acesso de dados seja feita por uma classe filha de Entity
+    {
+
+    }
+}
